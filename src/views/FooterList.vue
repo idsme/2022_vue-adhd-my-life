26c9d4b86880cc88600aa153b6f58c7682fd5192
idsme <idsmesales@gmail.com>
2022-05-10 23:51:13 +0200

    feature/create Footer list component.

---
+++ src/views/FooterList.vue
@@ -0,0 +1,369 @@
+<template>
+  <v-row>
+    <v-col cols="12" sm="4">
+      <v-card color="#22222C" class="rounded-lg" elevation="10">
+        <v-app-bar
+            flat
+            color="rgba(0, 0, 0, 0)"
+        >
+
+          <v-toolbar-title class="text-subtitle-2 white--text pl-0 ">
+            UX UI Design <br>
+            <span class="caption grey--text">Deans and Creative</span>
+          </v-toolbar-title>
+
+          <v-spacer></v-spacer>
+          <v-btn
+              color="#7C7C7F"
+              icon
+              small
+          >
+            <v-icon>mdi-dots-vertical</v-icon>
+          </v-btn>
+        </v-app-bar>
+        <v-app-bar
+            flat
+            color="rgba(0, 0, 0, 0)"
+            class="mt-n7"
+        >
+          <v-avatar size="30">
+            <img
+                src="https://cdn.vuetifyjs.com/images/lists/4.jpg"
+                alt="John"
+            >
+          </v-avatar>
+          <v-avatar class="ml-n3" size="30">
+            <img
+                src="https://cdn.vuetifyjs.com/images/lists/1.jpg"
+                alt="John"
+            >
+          </v-avatar>
+          <v-avatar class="ml-n3" size="30">
+            <img
+                src="https://cdn.vuetifyjs.com/images/lists/3.jpg"
+                alt="John"
+            >
+          </v-avatar>
+
+
+        </v-app-bar>
+        <v-app-bar
+            flat
+            color="rgba(0, 0, 0, 0)"
+            class="mt-n6"
+        >
+            <span
+                class="grey--text"
+                small
+            >
+              Progress
+            </span>
+          <v-spacer></v-spacer>
+          <span
+              class="caption grey--text"
+          >
+              45%
+            </span>
+        </v-app-bar>
+
+        <v-app-bar
+            flat
+            color="rgba(0, 0, 0, 0)"
+            class="mt-n10"
+        >
+          <v-progress-linear
+              color="green"
+              height="6"
+              value="45"
+
+          ></v-progress-linear>
+
+        </v-app-bar>
+        <v-card-text class="pt-0">
+          <v-row>
+            <v-col cols="12" sm="8">
+              <v-icon
+                  class="mr-2"
+                  small
+                  color="#7C7C7F"
+              >
+                fas fa-share-alt
+              </v-icon>
+              <span class="text-caption grey--text font-weight-light">5</span>
+              <v-icon
+                  class="ml-2 mr-2"
+                  small
+                  color="#7C7C7F"
+              >
+                far fa-comment-dots
+              </v-icon>
+              <span class="text-caption grey--text font-weight-light">3</span>
+            </v-col>
+            <v-col cols="12" sm="4">
+              <v-chip
+                  dark
+                  color="#7C7C7F"
+                  class=""
+                  small
+              >
+                <v-icon left small>
+                  mdi-alarm-check
+                </v-icon>
+                3 Week left
+              </v-chip>
+            </v-col>
+          </v-row>
+
+
+        </v-card-text>
+      </v-card>
+    </v-col>
+    <v-col cols="12" sm="4">
+      <v-card color="#22222C" class="rounded-lg" elevation="10">
+        <v-app-bar
+            flat
+            color="rgba(0, 0, 0, 0)"
+        >
+
+          <v-toolbar-title class="text-subtitle-2 white--text pl-0 ">
+            Backend Team <br>
+            <span class="caption grey--text">Vuejs Vuetify</span>
+          </v-toolbar-title>
+
+          <v-spacer></v-spacer>
+          <v-btn
+              color="#7C7C7F"
+              icon
+              small
+          >
+            <v-icon>mdi-dots-vertical</v-icon>
+          </v-btn>
+        </v-app-bar>
+        <v-app-bar
+            flat
+            color="rgba(0, 0, 0, 0)"
+            class="mt-n7"
+        >
+          <v-avatar size="30">
+            <img
+                src="https://cdn.vuetifyjs.com/images/lists/4.jpg"
+                alt="John"
+            >
+          </v-avatar>
+          <v-avatar class="ml-n3" size="30">
+            <img
+                src="https://cdn.vuetifyjs.com/images/lists/1.jpg"
+                alt="John"
+            >
+          </v-avatar>
+          <v-avatar class="ml-n3" size="30">
+            <img
+                src="https://cdn.vuetifyjs.com/images/lists/3.jpg"
+                alt="John"
+            >
+          </v-avatar>
+          <v-avatar class="ml-n3" size="30">
+            <img
+                src="https://cdn.vuetifyjs.com/images/lists/5.jpg"
+                alt="John"
+            >
+          </v-avatar>
+
+
+        </v-app-bar>
+        <v-app-bar
+            flat
+            color="rgba(0, 0, 0, 0)"
+            class="mt-n6"
+        >
+            <span
+                class="grey--text"
+                small
+            >
+              Progress
+            </span>
+          <v-spacer></v-spacer>
+          <span
+              class="caption grey--text"
+          >
+              95%
+            </span>
+        </v-app-bar>
+
+        <v-app-bar
+            flat
+            color="rgba(0, 0, 0, 0)"
+            class="mt-n10"
+        >
+          <v-progress-linear
+              color="green"
+              height="6"
+              value="95"
+
+          ></v-progress-linear>
+
+        </v-app-bar>
+        <v-card-text class="pt-0">
+          <v-row>
+            <v-col cols="12" sm="8">
+              <v-icon
+                  class="mr-2"
+                  small
+                  color="#7C7C7F"
+              >
+                fas fa-share-alt
+              </v-icon>
+              <span class="text-caption grey--text font-weight-light">5</span>
+              <v-icon
+                  class="ml-2 mr-2"
+                  small
+                  color="#7C7C7F"
+              >
+                far fa-comment-dots
+              </v-icon>
+              <span class="text-caption grey--text font-weight-light">3</span>
+            </v-col>
+            <v-col cols="12" sm="4">
+              <v-chip
+                  dark
+                  color="#7C7C7F"
+                  class=""
+                  small
+              >
+                <v-icon left small>
+                  mdi-alarm-check
+                </v-icon>
+                2 Week left
+              </v-chip>
+            </v-col>
+          </v-row>
+
+
+        </v-card-text>
+      </v-card>
+    </v-col>
+    <v-col cols="12" sm="4">
+      <v-card color="#22222C" class="rounded-lg" elevation="10">
+        <v-app-bar
+            flat
+            color="rgba(0, 0, 0, 0)"
+        >
+
+          <v-toolbar-title class="text-subtitle-2 white--text pl-0 ">
+            Software Testing <br>
+            <span class="caption grey--text">Developement</span>
+          </v-toolbar-title>
+
+          <v-spacer></v-spacer>
+          <v-btn
+              color="#7C7C7F"
+              icon
+              small
+          >
+            <v-icon>mdi-dots-vertical</v-icon>
+          </v-btn>
+        </v-app-bar>
+        <v-app-bar
+            flat
+            color="rgba(0, 0, 0, 0)"
+            class="mt-n7"
+        >
+          <v-avatar size="30">
+            <img
+                src="https://cdn.vuetifyjs.com/images/lists/4.jpg"
+                alt="John"
+            >
+          </v-avatar>
+          <v-avatar class="ml-n3" size="30">
+            <img
+                src="https://cdn.vuetifyjs.com/images/lists/1.jpg"
+                alt="John"
+            >
+          </v-avatar>
+          <v-avatar class="ml-n3" size="30">
+            <img
+                src="https://cdn.vuetifyjs.com/images/lists/3.jpg"
+                alt="John"
+            >
+          </v-avatar>
+
+
+        </v-app-bar>
+        <v-app-bar
+            flat
+            color="rgba(0, 0, 0, 0)"
+            class="mt-n6"
+        >
+            <span
+                class="grey--text"
+                small
+            >
+              Progress
+            </span>
+          <v-spacer></v-spacer>
+          <span
+              class="caption grey--text"
+          >
+              75%
+            </span>
+        </v-app-bar>
+
+        <v-app-bar
+            flat
+            color="rgba(0, 0, 0, 0)"
+            class="mt-n10"
+        >
+          <v-progress-linear
+              color="green"
+              height="6"
+              value="75"
+
+          ></v-progress-linear>
+
+        </v-app-bar>
+        <v-card-text class="pt-0">
+          <v-row>
+            <v-col cols="12" sm="8">
+              <v-icon
+                  class="mr-2"
+                  small
+                  color="#7C7C7F"
+              >
+                fas fa-share-alt
+              </v-icon>
+              <span class="text-caption grey--text font-weight-light">5</span>
+              <v-icon
+                  class="ml-2 mr-2"
+                  small
+                  color="#7C7C7F"
+              >
+                far fa-comment-dots
+              </v-icon>
+              <span class="text-caption grey--text font-weight-light">3</span>
+            </v-col>
+            <v-col cols="12" sm="4">
+              <v-chip
+                  dark
+                  color="#7C7C7F"
+                  class=""
+                  small
+              >
+                <v-icon left small>
+                  mdi-alarm-check
+                </v-icon>
+                2 Week left
+              </v-chip>
+            </v-col>
+          </v-row>
+
+
+        </v-card-text>
+      </v-card>
+    </v-col>
+  </v-row>
+</template>
+<script>
+export default {
+  name: 'FooterList'
+}
+</script>
