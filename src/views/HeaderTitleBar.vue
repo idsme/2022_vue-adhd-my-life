44ecd252ee4ddb971eb7daa0d3e509d64af8e0a7
idsme <idsmesales@gmail.com>
2022-05-10 23:55:00 +0200

    /feature/extract HeaderTitleBar

---
+++ src/views/HeaderTitleBar.vue
@@ -0,0 +1,10 @@
+<template>
+  <v-toolbar color="rgba(0,0,0,0)" flat>
+    <v-toolbar-title class="mt-n4 white--text">Task Management</v-toolbar-title>
+  </v-toolbar>
+</template>
+<script>
+export default {
+  name: 'HeaderTitleBar'
+}
+</script>
